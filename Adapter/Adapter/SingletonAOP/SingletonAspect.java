29049ab4c26e11b307bb56d7f8b22a6065192cd0
Domaalva <139288690+Domaalva@users.noreply.github.com>
2025-06-06 01:18:53 -0500

    Create SingletonAspect.java

---
+++ Adapter/Adapter/SingletonAOP/SingletonAspect.java
@@ -0,0 +1,20 @@
+package Adapter.SingletonAOP;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.ProceedingJoinPoint;
+
+@Aspect
+public class SingletonAspect {
+    private ConexionBaseDeDatos instancia;
+
+    @Around("execution(Adapter.SingletonAOP.ConexionBaseDeDatos.new(..)) && args(cadenaConexion)")
+    public Object interceptarConstructor(ProceedingJoinPoint pjp, String cadenaConexion) throws Throwable {
+        if (instancia == null) {
+            instancia = new ConexionBaseDeDatos(cadenaConexion);
+        } else {
+            System.out.println("Ya existe una instancia. Usando la existente.");
+        }
+        return instancia;
+    }
+}
