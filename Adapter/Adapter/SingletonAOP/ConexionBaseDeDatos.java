51b14267958fb16b01aaab81f9e41a83a835947f
Domaalva <139288690+Domaalva@users.noreply.github.com>
2025-06-06 01:08:14 -0500

    Update ConexionBaseDeDatos.java

--- Adapter/Adapter/SingletonAOP/ConexionBaseDeDatos.java
+++ Adapter/Adapter/SingletonAOP/ConexionBaseDeDatos.java
@@ -1,1 +1,15 @@
+package Adapter.SingletonAOP;
+
+public class ConexionBaseDeDatos {
+    private String cadenaConexion;
+
+    public ConexionBaseDeDatos(String cadenaConexion) {
+        this.cadenaConexion = cadenaConexion;
+        System.out.println("Conectando a la base de datos con: " + cadenaConexion);
+    }
+
+    public void ejecutarConsulta(String consulta) {
+        System.out.println("Ejecutando consulta: " + consulta);
+    }
+}
 
